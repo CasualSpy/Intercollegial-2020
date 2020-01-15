8c18340eed74ecc64d352acddf24fd937c7b8959
EBhero <etiennebrisson@hotmail.com>
2020-01-15 11:41:19 -0500

    VIDE CHECK

---
+++ Unity Project/Assets/DialogueManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using VIDE_Data;
+using System;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject container_NPC;
+    public GameObject container_PLAYER;
+    public TextMeshProUGUI text_NPC;
+    public TextMeshProUGUI[] text_Choices;
+    public VIDE_Assign CurrentNPC;
+    PlayerData playerData;
+    // Start is called before the first frame update
+    // Use this for initialization
+    void Start()
+    {
+        playerData = GetComponent<PlayerData>();
+        VD.LoadDialogues();
+
+
+        container_NPC.SetActive(false);
+        container_PLAYER.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Return) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        //{
+        //    if (!VD.isActive)
+        //    {
+        //        Begin();
+        //    }
+        //    else
+        //    {
+        //        Next();
+        //    }
+        //}
+    }
+
+    public void Next()
+    {
+        VD.Next();
+    }
+
+    public void Begin()
+    {
+        VD.OnNodeChange += UpdateUI;
+        VD.OnEnd += End;
+        VD.BeginDialogue(CurrentNPC);
+    }
+
+    void UpdateUI(VD.NodeData data)
+    {
+        container_NPC.SetActive(false);
+        container_PLAYER.SetActive(false);
+        if (data.isPlayer)
+        {
+            container_PLAYER.SetActive(true);
+            for (int i = 0; i < text_Choices.Length; i++)
+            {
+                if (i < data.comments.Length)
+                {
+                    text_Choices[i].transform.parent.gameObject.SetActive(true);
+                    text_Choices[i].text = data.comments[i];
+                }
+                else
+                {
+                    text_Choices[i].transform.parent.gameObject.SetActive(false);
+                }
+            }
+            text_Choices[0].transform.parent.GetComponent<UnityEngine.UI.Button>().Select();
+        }
+        else
+        {
+            container_NPC.SetActive(true);
+            text_NPC.text = data.comments[data.commentIndex];
+            //Play Audio if any
+            //if (data.audios[data.commentIndex] != null)
+            //    audioSource.clip = data.audios[data.commentIndex];
+            //audioSource.Play();
+        }
+
+        foreach (var item in data.extraVars)
+        {
+            playerData.Events(item.Key, item.Value.ToString());
+        }
+
+        if (data.sprite != null)
+            playerData.TalkingTo.SetImage(data.sprite);
+    }
+
+
+    void End(VD.NodeData data)
+    {
+        container_NPC.SetActive(false);
+        container_PLAYER.SetActive(false);
+        VD.OnNodeChange -= UpdateUI;
+        VD.OnEnd -= End;
+        VD.EndDialogue();
+    }
+
+    void OnDisable()
+    {
+        if (container_NPC != null)
+            End(null);
+    }
+
+    public void SetPlayerChoice(int choice)
+    {
+        VD.nodeData.commentIndex = choice;
+        if (Input.GetMouseButtonUp(0))
+            VD.Next();
+    }
+}
--- Unity Project/Assets/PlayerData.cs
+++ Unity Project/Assets/PlayerData.cs
@@ -9,14 +9,16 @@ public class PlayerData : MonoBehaviour
 {
     private static PlayerData instance = null;
     private bool InDialog = false;
-    private Dateable TalkingTo;
+    public Dateable TalkingTo;
     private bool WaitForInput = false;
+    DialogueManager dialogueManager;
     public enum Item
     {
     }
 
     private void Start()
     {
+        dialogueManager = GetComponent<DialogueManager>();
     }
 
     public List<Item> Inventory { get; set; }
@@ -39,8 +41,11 @@ public class PlayerData : MonoBehaviour
         Debug.Log("Starting dialog with " + dateable.Name);
         TalkingTo = dateable;
 
-        TalkingTo.dialogues.Reset();
-        ReadCurrentDialog();
+        dialogueManager.CurrentNPC = dateable.VIDE;
+        dialogueManager.Begin();
+
+        //TalkingTo.dialogues.Reset();
+        //ReadCurrentDialog();
     }
 
     void HandleTriggers(string trigger)
@@ -53,77 +58,89 @@ public class PlayerData : MonoBehaviour
             {
                 string triggerName = match.Groups[i].Value;
                 string value = match.Groups[i + 1].Value;
-
-                switch(triggerName.ToLower())
-                {
-                    // Change NPC sprite to display another emotion
-                    case "emotion":
-                        TalkingTo.SetEmotion(value);
-                        break;
-                    // Add to NPC suspicion value (negative value to remove)
-                    case "suspicion":
-                        float parsed = 0f;
-                        if (float.TryParse(value, out parsed))
-                            TalkingTo.Suspicion += parsed;
-                        break;
-                    // Set new dialog tree
-                    case "tree":
-                        TalkingTo.GetComponent<Dialogues>().SetTree(value);
-                        break;
-                    // Add new item to inventory
-                    case "addInventory":
-                        Inventory.Add((Item)Enum.Parse(typeof(Item), value));
-                        break;
-                    // Remove item from inventory
-                    case "removeInventory":
-                        Inventory.Remove((Item)Enum.Parse(typeof(Item), value));
-                        break;
-                    // Add to player's gold (negative value to remove
-                    case "gold":
-                        int parsedGold = 0;
-                        if (int.TryParse(value, out parsedGold))
-                            Gold += parsedGold;
-                        break;
-                    // Change background sprite
-                    case "background":
-                        GameObject.Find("Background").GetComponent<BackgroundController>().SetBackground(value);
-                        break;
-                    // Change speaker name during dialog
-                    case "speaker":
-                        GameObject.Find("TextBox").GetComponent<DialogueWindowScript>().SetSpeaker(value);
-                        break;
-                }
+                Events(triggerName, value);
             }
+        }
+    }
+
+    public void Events(string name, string value)
+    {
+        switch (name.ToLower())
+        {
+            // Change NPC sprite to display another emotion
+            case "emotion":
+                TalkingTo.SetEmotion(value);
+                break;
+            // Add to NPC suspicion value (negative value to remove)
+            case "suspicion":
+                float parsed = 0f;
+                if (float.TryParse(value, out parsed))
+                    TalkingTo.Suspicion += parsed;
+                break;
+            // Set new dialog tree
+            case "tree":
+                TalkingTo.GetComponent<Dialogues>().SetTree(value);
+                break;
+            // Add new item to inventory
+            case "addInventory":
+                Inventory.Add((Item)Enum.Parse(typeof(Item), value));
+                break;
+            // Remove item from inventory
+            case "removeInventory":
+                Inventory.Remove((Item)Enum.Parse(typeof(Item), value));
+                break;
+            // Add to player's gold (negative value to remove
+            case "gold":
+                int parsedGold = 0;
+                if (int.TryParse(value, out parsedGold))
+                    Gold += parsedGold;
+                break;
+            // Change background sprite
+            case "background":
+                GameObject.Find("Background").GetComponent<BackgroundController>().SetBackground(value);
+                break;
+            // Change speaker name during dialog
+            case "speaker":
+                GameObject.Find("TextBox").GetComponent<DialogueWindowScript>().SetSpeaker(value);
+                break;
         }
     }
 
     public void NextDialog()
     {
-        TalkingTo.dialogues.Next();
-        ReadCurrentDialog();
+        dialogueManager.Next();
+
+        //TalkingTo.dialogues.Next();
+        //ReadCurrentDialog();
     }
 
     void ReadCurrentDialog()
     {
-        string dialog = TalkingTo.dialogues.GetCurrentDialogue();
-        GameObject.Find("TextBox").GetComponent<DialogueWindowScript>().SetText(dialog);
-        if (TalkingTo.dialogues.HasTrigger())
-            HandleTriggers(TalkingTo.dialogues.GetTrigger());
+        //string dialog = TalkingTo.dialogues.GetCurrentDialogue();
+        //GameObject.Find("TextBox").GetComponent<DialogueWindowScript>().SetText(dialog);
+        //Dialogues dialogues = TalkingTo.dialogues;
+        //if (TalkingTo.dialogues.HasTrigger())
+        //    HandleTriggers(TalkingTo.dialogues.GetTrigger());
 
-        string[] choices = TalkingTo.dialogues.GetChoices();
+        //string[] choices = TalkingTo.dialogues.GetChoices();
 
-        if (choices.Length > 0)
-        {
-            //Decisions!
-            GameObject.Find("ChoiceBox").GetComponent<DecisionWindow>().PromptUser(choices[0], choices[1], choices[2]);
-            WaitForInput = true;
-        }
+        //if (choices.Length > 0)
+        //{
+        //    //Decisions!
+        //    GameObject.Find("ChoiceBox").GetComponent<DecisionWindow>().PromptUser(choices[0], choices[1], choices[2]);
+        //    WaitForInput = true;
+        //}
     }
 
     public void RecieveChoice(string choice)
     {
-        WaitForInput = false;
-        TalkingTo.dialogues.NextChoice(choice);
-        ReadCurrentDialog();
+        //WaitForInput = false;
+        //TalkingTo.dialogues.NextChoice(choice);
+        //ReadCurrentDialog();
+    }
+
+    public void ShowBook()
+    {
+
     }
 }
