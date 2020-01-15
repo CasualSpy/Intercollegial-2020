1148fa5c4ccb507df5818d44b2042b8f7792e7b2
CasualSpy <arnaudbegin@hotmail.com>
2020-01-15 15:18:34 -0500

    Fix error in ShopManager

--- Unity Project/Assets/ShopManager.cs
+++ Unity Project/Assets/ShopManager.cs
@@ -15,7 +15,7 @@ public class ShopManager : MonoBehaviour
         if (player.Gold >= item.Price)
         {
             player.Gold -= item.Price;
-            player.Inventory.Add(item.Item);
+            player.AddInventoryItem(item.Item);
             GameObject.Find("Description").GetComponent<TextMeshProUGUI>().text = $"Vous achetez 1 {item.Item.ToString()}";
         }
         else
