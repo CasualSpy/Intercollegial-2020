8db9e12d29e57b063bfd8b65a45ebbe66ecae1e5
CasualSpy <arnaudbegin@hotmail.com>
2020-01-14 21:15:05 -0500

    Start HandleTriggers method

--- Unity Project/Assets/PlayerData.cs
+++ Unity Project/Assets/PlayerData.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
+using System;
+using UnityEngine.UI;
 
-public class PlayerData
+public class PlayerData : MonoBehaviour
 {
     private static PlayerData instance = null;
+    private bool InDialog = false;
+    private Dateable TalkingTo;
     public enum Item
+    {
+    }
+
+    private void Start()
     {
     }
 
@@ -22,5 +31,53 @@ public class PlayerData
         if (instance == null)
             instance = new PlayerData();
         return instance;
+    }
+
+    void StartDialog(Dateable dateable)
+    {
+        TalkingTo = dateable;
+
+    }
+
+    void HandleTriggers(string trigger)
+    {
+        Regex rx = new Regex("([\\w\\d]+):([\\w\\d]+);");
+        MatchCollection matches = rx.Matches(trigger);
+        foreach (Match match in matches)
+        {
+            for (int i = 1; i < match.Groups.Count; i+=2)
+            {
+                string triggerName = match.Groups[i].Value;
+                string value = match.Groups[i + 1].Value;
+
+                switch(triggerName)
+                {
+                    case "emotion":
+                        TalkingTo.SetEmotion(value);
+                        break;
+                    case "suspicion":
+                        float parsed = 0f;
+                        if (float.TryParse(value, out parsed))
+                            TalkingTo.Suspicion += parsed;
+                        break;
+                    case "tree":
+                        TalkingTo.GetComponent<Dialogues>().SetTree(value);
+                        break;
+                    case "addInventory":
+                        Inventory.Add((Item)Enum.Parse(typeof(Item), value));
+                        break;
+                    case "removeInventory":
+                        Inventory.Remove((Item)Enum.Parse(typeof(Item), value));
+                        break;
+                    case "gold":
+                        int parsedGold = 0;
+                        if (int.TryParse(value, out parsedGold))
+                            Gold += parsedGold;
+                        break;
+                    case "background":
+                        break;
+                }
+            }
+        }
     }
 }
