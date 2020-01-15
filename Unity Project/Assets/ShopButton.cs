279960f5c5b0d449b50b0036ca9d46d2213988fa
CasualSpy <arnaudbegin@hotmail.com>
2020-01-15 11:32:32 -0500

    Create shop for real this time

---
+++ Unity Project/Assets/ShopButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class ShopButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public ShopItem Item;
+
+    private void Start()
+    {
+        GetComponentInChildren<TextMeshProUGUI>().text = $"{Item.Item} - {Item.Price}$";
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GameObject.Find("Description").GetComponent<TextMeshProUGUI>().text = Item.Description;
+        GameObject.Find("ItemName").GetComponent<TextMeshProUGUI>().text = Item.Item.ToString();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GameObject.Find("Description").GetComponent<TextMeshProUGUI>().text =  "Vous trouverez ici tout ce dont vous avez de besoin.";
+        GameObject.Find("ItemName").GetComponent<TextMeshProUGUI>().text = "Magasin général";
+    }
+}
