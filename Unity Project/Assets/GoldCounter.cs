41631adbe14b6494c88135ebb1d19873bd0f3dfc
CasualSpy <arnaudbegin@hotmail.com>
2020-01-15 22:34:59 -0500

    Actually add gold counter

---
+++ Unity Project/Assets/GoldCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldCounter : MonoBehaviour
+{
+    TMPro.TextMeshProUGUI textBox;
+    PlayerData playerData;
+
+    void Start()
+    {
+        textBox = GetComponent<TMPro.TextMeshProUGUI>();
+        playerData = GameObject.Find("Player").GetComponent<PlayerData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textBox.text = $"Argent: {playerData.Gold}$";
+    }
+}
